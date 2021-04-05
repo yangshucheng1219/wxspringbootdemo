913d181ec77efe09a3f26896a7cbb1611afb2e42
yangshucheng2020 <yangshucheng1219@foxmail.com>
2021-04-05 14:17:05 +0800

    test

--- src/main/java/com/imooc/demo/web/AreaController.java
+++ src/main/java/com/imooc/demo/web/AreaController.java
@@ -53,6 +53,7 @@ public class AreaController {
     }
 
     //主要研究前后端都用DELETE请求的方法出错的问题
+    //test
     @RequestMapping(value = "/removearea",method = RequestMethod.DELETE)
     private Map<String, Object> removeArea(Integer areaId){
         Map<String, Object> modelMap = new HashMap<>();
